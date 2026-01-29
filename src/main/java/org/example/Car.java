04a638579534666814d857fe571c3b651f008782
charliegonzo <gonzc3@farmingdale.edu>
2026-01-29 13:35:13 -0500

    Created Car class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,46 @@
+package org.example;
+
+/**
+ * @author Charles Gonzalez
+ * Car class extends {@link Vehicle} class, adding extra functionality.
+ */
+public class Car extends Vehicle{
+
+    String brand;
+
+    // Constructor
+    public Car(int numberofWheels, String color, float engineSize, String fuelftype,String brand) {
+        super(numberofWheels, color, engineSize, fuelftype);
+        this.brand = brand;
+    }
+
+    // getter and setter
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * This function just Honks...
+     */
+    public void honk(){
+        System.out.println("Honk, honk!");
+    }
+
+    /**
+     * Prints out Car info.
+     */
+    public void displayInfo(){
+        System.out.println("Car{" +
+                "brand='" + brand + '\'' +
+                ", numberofWheels=" + numberofWheels +
+                ", color='" + color + '\'' +
+                ", engineSize=" + engineSize +
+                ", fuelftype='" + fuelftype + '\'' +
+                '}');
+    }
+
+} // End of class
